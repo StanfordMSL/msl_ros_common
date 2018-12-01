378924dc0952f68a493615860f7fcc50501d5013
Michael Kossyrev <mkossyre@stanford.edu>
2018-11-30 17:07:54 -0800

    boundary_stripped now does 1/2 second propagation of position overseer_stripped_sim works in simulator - uses new emergency service overseer_stripped_live should be used in flightroom overseer* will now properly remove quads that drop out of roscore

--- quad_safety/scripts/boundary_stripped.py
+++ quad_safety/scripts/boundary_stripped.py
@@ -4,7 +4,7 @@ import rospy
 import tf
 import std_msgs.msg
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped, Pose, TwistStamped
 from mavros_msgs.srv import SetMode, CommandBool
 from mavros_msgs.msg import State, ParamValue
 from sensor_msgs.msg import BatteryState
@@ -25,9 +25,11 @@ class Safety:
 
 		#initialize variables of interest
 		self.refresh_rate = 10
+		self.propagation_time = 0.5 #if colliding with room in half a second (using linear d(t+0.5) = d(t) + v*0.5)
 		self.bounds = np.array(rospy.get_param('room_boundaries')) #first row - lower bounds; second row - upper bounds
 		self.landing_pose = Pose()
 		self.position = None
+		self.velocity = np.zeros((3,1))
 		self.battery_level = None
 
 		self.quad_name = rospy.get_namespace()
@@ -40,6 +42,7 @@ class Safety:
 
 		self.state_sub = rospy.Subscriber('mavros/local_position/pose',PoseStamped,self.agentPoseCB)
 		self.battery_sub = rospy.Subscriber('mavros/battery',BatteryState,self.agentBatteryVoltage)
+		self.velocity_sub = rospy.Subscriber('mavros/local_position/velocity',TwistStamped,self.agentVelocity)
 
 		while self.position is None:
 			rospy.loginfo("Waiting for pose")
@@ -62,6 +65,24 @@ class Safety:
 	def agentBatteryVoltage(self,msg):
 		self.battery_level = msg.voltage
 
+	def agentVelocity(self,msg):
+		vel = msg.twist.linear
+		self.velocity[0] = vel.x 
+		self.velocity[1] = vel.y
+		self.velocity[2] = vel.z 
+
+	def collectVelocities(self,msg):
+		experimental = False
+		if self.all_velocities.shape[1] <= quad_idx: #number of cols < number of quads
+			num_cols = self.all_velocities.shape[1]
+			self.all_velocities = np.hstack((self.all_velocities, np.zeros((3, quad_idx-num_cols+1)))) #resize-as-you-go
+
+		vel = msg.twist.linear
+		self.all_velocities[:,quad_idx] = np.array([vel.x, vel.y, vel.z])
+
+		effective_pos = self.all_positions + self.propagation_time*self.all_velocities #Propagate position by propagation_time
+		quad_norms = np.square(np.linalg.norm(effective_pos,axis=0))
+
 	def run(self):
 		rate = rospy.Rate(self.refresh_rate)
 		debug_text = ""
@@ -71,7 +92,9 @@ class Safety:
 				rospy.loginfo("Battery Voltage too low")
 				self.land_service(True, self.landing_pose,None)
 
-			if ((self.position < self.bounds[0]).any() or (self.position > self.bounds[1]).any()):
+			propagated_position = self.position + self.propagation_time*self.velocity
+
+			if ((propagated_position < self.bounds[0]).any() or (propagated_position > self.bounds[1]).any()):
 				debug_text = np.array2string(self.position)
 				rospy.loginfo(debug_text)
 				self.land_service(True,self.landing_pose,None)
